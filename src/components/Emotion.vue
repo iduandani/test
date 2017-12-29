4304006275dc5860f7a351e5790b14e17bbdf6d3
dan.duan <dan.duan@sao.so>
2017-12-29 16:04:16 +0800

    123123

--- src/components/Emotion.vue
+++ src/components/Emotion.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Emotion">
-    <div class="title">情绪1312</div>
+    <div class="title">情绪1355512</div>
     <form id="uploadForm" method="post" enctype="multipart/form-data">
 
             <div class="img-container" id='pic_c'>
